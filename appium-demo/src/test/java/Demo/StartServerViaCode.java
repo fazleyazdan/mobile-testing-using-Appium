746f36b9fcc33ad300bd6e03d5bf8a2e89a83d57
fazleyazdan <fazleyazdan345@gmail.com>
2024-07-10 17:22:52 +0500

    Run Server Programmatically

--- appium-demo/src/test/java/Demo/StartServerViaCode.java
+++ appium-demo/src/test/java/Demo/StartServerViaCode.java
@@ -1,26 +1,37 @@
 package Demo;
 
+import java.io.File;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-public class InstallApk {
+public class StartServerViaCode {
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+	
+	public void configureAppium() throws MalformedURLException
+	
+	{
+		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\Fazle Yazdan\\AppData\\Roaming\\npm\\node_modules\\appium\\lib\\main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		
+		service.start();
 		
 		// Create uiautomator2 object and set the capabilities
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("Pixel 3a API 34");					// we pass the emulator name here
 		options.setApp("D:\\Apps\\apk files\\UI Demo App.apk");		// path where the apk is located
-		
+				
 		// create object for AndroidDriver/IOSDriver		
 		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);	// appium server address	
-		// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(13));
-		Thread.sleep(7000);
-		driver.quit();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.quit();		
+		
+		service.stop();
 	}
-
 }
