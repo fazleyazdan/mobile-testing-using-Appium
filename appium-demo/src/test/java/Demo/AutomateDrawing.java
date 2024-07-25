84c368442351a590d719fa93f4d8d90682d44a4c
fazleyazdan <fazleyazdan345@gmail.com>
2024-07-25 12:12:32 +0500

    Handling Drawing

--- appium-demo/src/test/java/Demo/AutomateDrawing.java
+++ appium-demo/src/test/java/Demo/AutomateDrawing.java
@@ -36,28 +36,36 @@ public class AutomateDrawing {
 		
 		WebElement drawing_el = driver.findElement(By.xpath("//android.webkit.WebView"));
 		
+		// get location of the element
+		Point location = drawing_el.getLocation();
+		// get size of the element
 		Dimension size = drawing_el.getSize();
 		
-		int start_x = size.getWidth()/2;
-		int start_y = (int) (size.getHeight() * 0.25);
+		// Position where you wanna touch your finger to start drawing from
+		int start_x = (location.x + size.getWidth()/2);
+		int start_y = (int) (location.y + size.getHeight() * 0.25);
+		
+		// Position where you wanna end your drawing
 		int end_x = start_x;
-		int end_y = (int) (size.getHeight() * 0.75);
+		int end_y = (location.y + size.getHeight());
+//		int end_y = (int) (location.y + size.getHeight() * 0.75);
+		
 		
 		// PointerInput: Represents an input device (e.g., touch, mouse, pen).
-        // In our code, PointerInput is created to represent a touch input device.
+        // In our case, PointerInput is created to represent a touch input device.
         PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");  
         // Here in argument we specified which kind of action it is. i.e. Mouse, Touch, Pen etc.
         
         // The Sequence object in Selenium WebDriver and Appium is used to define a series of input actions that can be performed in a sequence.
         // This allows you to create complex user interactions that involve multiple steps, such as dragging and dropping, multi-touch gestures etc.
         Sequence sequence = new Sequence(finger, 1)      // 1 is the id of the touch, you can give any id of Your choice
-        		// Moves the pointer (finger) to the center of the screen
+        		// Moves the pointer (finger) to the center of the screen.
                 .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), start_x, start_y))
-                // Simulates pressing down on the center of the screen.
+                // Simulates pressing down on the screen
                 .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
                 // Adds a pause for 200 milliseconds to simulate the finger holding down on the screen.
                 .addAction(new Pause(finger, Duration.ofMillis(200)))
-                // Moves the pointer to the end of the y-axis or ending point of scrolling over a duration of 200 milliseconds.
+                // Moves the pointer to the end of the y-axis or ending point of drawing over a duration of 200 milliseconds.
                 .addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), end_x, end_y))
                 // Simulates lifting the finger off the screen
                 .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
