b2efebcb9effc9e233eb3d33034ff5d27fbb5711
fazleyazdan <fazleyazdan345@gmail.com>
2024-07-29 12:24:56 +0500

    Setting up Base Class

---
+++ appium-demo/src/test/java/Demo/BaseClass.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class BaseClass {
+	
+	// we will keep Desired Capabilities Here
+	public void setup()
+	{
+		
+	}
+	
+	// we will keep session ending script here
+	public void tearDown()
+	{
+		
+	}
+	
+		
+
+}
