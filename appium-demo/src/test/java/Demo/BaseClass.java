3e47fe168610599033959433cb5c36ed57d8c93a
fazleyazdan <fazleyazdan345@gmail.com>
2024-07-29 12:55:29 +0500

    Base Class in Appium

--- appium-demo/src/test/java/Demo/BaseClass.java
+++ appium-demo/src/test/java/Demo/BaseClass.java
@@ -1,17 +1,39 @@
 package Demo;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
 public class BaseClass {
 	
+	AndroidDriver driver;
+	
 	// we will keep Desired Capabilities Here
-	public void setup()
+	@BeforeTest
+	public void setup() throws MalformedURLException
 	{
-		
+		// Here we created this setup method specifically to automate web browsing
+		UiAutomator2Options options = new UiAutomator2Options()
+				.setDeviceName("Pixel 3a API 34") // Change to your emulator/device name
+				.withBrowserName("Chrome")
+				.setPlatformName("Android")
+				.setAutomationName("UiAutomator2")
+				.setChromedriverExecutable("C:\\Drivers\\appium_driver\\chromedriver.exe");
+
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
 	}
 	
 	// we will keep session ending script here
+	@AfterTest
 	public void tearDown()
 	{
-		
+		driver.close();
+		driver.quit();
 	}
 	
 		
