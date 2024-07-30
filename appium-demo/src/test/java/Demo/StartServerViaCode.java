381f5722b347d505675238d52ba3fdb05ad9b353
fazleyazdan <fazleyazdan345@gmail.com>
2024-07-30 12:58:28 +0500

    Start Server Via Code

--- appium-demo/src/test/java/Demo/StartServerViaCode.java
+++ appium-demo/src/test/java/Demo/StartServerViaCode.java
@@ -7,6 +7,8 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.testng.annotations.Test;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
